ba6b2ae0de3f3d42220cc99554b507ce5c4b7a4c
Jayantaphotography <paljayanta18@gmail.com>
2022-07-23 18:40:50 +0530

    final data

---
+++ java script/java script.js
@@ -0,0 +1,913 @@
+const hamburger = document .querySelector('.hamburger');
+const navlist = document .querySelector('.navlist');
+
+hamburger.addEventListener('click',()=>{
+    navlist.classList.toggle('show');
+} )
+
+
+
+var likeCount = document.querySelector('#likeCount');
+var dislikeCount = document.querySelector('#dislikeCount');
+
+function count(){
+  likeCount.value = parseInt(likeCount.value) + 1;
+}
+
+function count1(){
+  dislikeCount.value = parseInt(dislikeCount.value) + 1;
+}
+
+
+function count2(){
+    likeCount2.value = parseInt(likeCount2.value) + 1;
+  }
+  
+  function count3(){
+    dislikeCount3.value = parseInt(dislikeCount3.value) + 1;
+  }
+
+  
+function count4(){
+    likeCount4.value = parseInt(likeCount4.value) + 1;
+  }
+  
+  function count5(){
+    dislikeCount5.value = parseInt(dislikeCount5.value) + 1;
+  }
+
+  
+
+function count6(){
+    likeCount6.value = parseInt(likeCount6.value) + 1;
+  }
+  
+  function count7(){
+    dislikeCount7.value = parseInt(dislikeCount7.value) + 1;
+  }
+
+  function count8(){
+    likeCount8.value = parseInt(likeCount8.value) + 1;
+  }
+  
+  function count9(){
+    dislikeCount9.value = parseInt(dislikeCount9.value) + 1;
+  }
+
+  function count10(){
+    likeCount10.value = parseInt(likeCount10.value) + 1;
+  }
+  
+  function count11(){
+    dislikeCount11.value = parseInt(dislikeCount11.value) + 1;
+  }
+
+  function count12(){
+    likeCount12.value = parseInt(likeCount12.value) + 1;
+  }
+  
+  function count13(){
+    dislikeCount13.value = parseInt(dislikeCount13.value) + 1;
+  }
+
+
+  function count14(){
+    likeCount14.value = parseInt(likeCount14.value) + 1;
+  }
+  
+  function count15(){
+    dislikeCount15.value = parseInt(dislikeCount15.value) + 1;
+  }
+
+  function count16(){
+    likeCount16.value = parseInt(likeCount16.value) + 1;
+  }
+  
+  function count17(){
+    dislikeCount17.value = parseInt(dislikeCount17.value) + 1;
+  }
+
+  function count18(){
+    likeCount18.value = parseInt(likeCount18.value) + 1;
+  }
+  
+  function count19(){
+    dislikeCount19.value = parseInt(dislikeCount19.value) + 1;
+  }
+
+  function count20(){
+    likeCount20.value = parseInt(likeCount20.value) + 1;
+  }
+  
+  function count21(){
+    dislikeCount21.value = parseInt(dislikeCount21.value) + 1;
+  }
+
+  function count22(){
+    likeCount22.value = parseInt(likeCount22.value) + 1;
+  }
+  
+  function count23(){
+    dislikeCount23.value = parseInt(dislikeCount23.value) + 1;
+  }
+
+  function count24(){
+    likeCount24.value = parseInt(likeCount24.value) + 1;
+  }
+  
+  function count25(){
+    dislikeCount25.value = parseInt(dislikeCount25.value) + 1;
+  }
+
+  function count26(){
+    likeCount26.value = parseInt(likeCount26.value) + 1;
+  }
+  
+  function count27(){
+    dislikeCount27.value = parseInt(dislikeCount27.value) + 1;
+  }
+
+  function count28(){
+    likeCount28.value = parseInt(likeCount28.value) + 1;
+  }
+  
+  function count29(){
+    dislikeCount29.value = parseInt(dislikeCount29.value) + 1;
+  }
+
+  function count30(){
+    likeCount30.value = parseInt(likeCount30.value) + 1;
+  }
+  
+  function count31(){
+    dislikeCount31.value = parseInt(dislikeCount31.value) + 1;
+  }
+
+  function count32(){
+    likeCount32.value = parseInt(likeCount32.value) + 1;
+  }
+  
+  function count33(){
+    dislikeCount33.value = parseInt(dislikeCount33.value) + 1;
+  }
+
+    
+function count34(){
+  likeCount34.value = parseInt(likeCount34.value) + 1;
+}
+
+function count35(){
+  dislikeCount35.value = parseInt(dislikeCount35.value) + 1;
+}
+
+function count36(){
+  likeCount36.value = parseInt(likeCount36.value) + 1;
+}
+
+function count37(){
+  dislikeCount37.value = parseInt(dislikeCount37.value) + 1;
+}
+
+function count38(){
+  likeCount38.value = parseInt(likeCount38.value) + 1;
+}
+
+function count39(){
+  dislikeCount39.value = parseInt(dislikeCount39.value) + 1;
+}
+
+function count40(){
+  likeCount40.value = parseInt(likeCount40.value) + 1;
+}
+
+function count41(){
+  dislikeCount41.value = parseInt(dislikeCount41.value) + 1;
+}
+
+function count42(){
+  likeCount42.value = parseInt(likeCount42.value) + 1;
+}
+
+function count43(){
+  dislikeCount43.value = parseInt(dislikeCount43.value) + 1;
+}
+
+function count44(){
+  likeCount44.value = parseInt(likeCount44.value) + 1;
+}
+
+function count45(){
+  dislikeCount45.value = parseInt(dislikeCount45.value) + 1;
+}
+
+function count46(){
+  likeCount46.value = parseInt(likeCount46.value) + 1;
+}
+
+function count47(){
+  dislikeCount47.value = parseInt(dislikeCount47.value) + 1;
+}
+
+function count48(){
+  likeCount48.value = parseInt(likeCount48.value) + 1;
+}
+
+function count49(){
+  dislikeCount49.value = parseInt(dislikeCount49.value) + 1;
+}
+
+function count50(){
+  likeCount50.value = parseInt(likeCount50.value) + 1;
+}
+
+function count51(){
+  dislikeCount51.value = parseInt(dislikeCount51.value) + 1;
+}
+
+function count52(){
+  likeCount52.value = parseInt(likeCount52.value) + 1;
+}
+
+function count53(){
+  dislikeCount53.value = parseInt(dislikeCount53.value) + 1;
+}
+
+function count54(){
+  likeCount54.value = parseInt(likeCount54.value) + 1;
+}
+
+function count55(){
+  dislikeCount55.value = parseInt(dislikeCount55.value) + 1;
+}
+
+function count56(){
+  likeCount56.value = parseInt(likeCount56.value) + 1;
+}
+
+function count57(){
+  dislikeCount57.value = parseInt(dislikeCount57.value) + 1;
+}
+
+function count58(){
+  likeCount58.value = parseInt(likeCount58.value) + 1;
+}
+
+function count59(){
+  dislikeCount59.value = parseInt(dislikeCount59.value) + 1;
+}
+
+function count60(){
+  likeCount60.value = parseInt(likeCount60.value) + 1;
+}
+
+function count61(){
+  dislikeCount61.value = parseInt(dislikeCount61.value) + 1;
+}
+
+function count62(){
+  likeCount62.value = parseInt(likeCount62.value) + 1;
+}
+
+function count63(){
+  dislikeCount63.value = parseInt(dislikeCount63.value) + 1;
+}
+
+function count64(){
+  likeCount64.value = parseInt(likeCount64.value) + 1;
+}
+
+function count65(){
+  dislikeCount65.value = parseInt(dislikeCount65.value) + 1;
+}
+
+function count66(){
+  likeCount66.value = parseInt(likeCount66.value) + 1;
+}
+
+function count67(){
+  dislikeCount67.value = parseInt(dislikeCount67.value) + 1;
+}
+
+function count68(){
+  likeCount68.value = parseInt(likeCount68.value) + 1;
+}
+
+function count69(){
+  dislikeCount69.value = parseInt(dislikeCount69.value) + 1;
+}
+function count70(){
+  likeCount70.value = parseInt(likeCount70.value) + 1;
+}
+
+function count71(){
+  dislikeCount71.value = parseInt(dislikeCount71.value) + 1;
+}
+
+function count72(){
+  likeCount72.value = parseInt(likeCount72.value) + 1;
+}
+
+function count73(){
+  dislikeCount73.value = parseInt(dislikeCount73.value) + 1;
+}
+
+function count74(){
+  likeCount74.value = parseInt(likeCount74.value) + 1;
+}
+
+function count75(){
+  dislikeCount75.value = parseInt(dislikeCount75.value) + 1;
+}
+
+function count76(){
+  likeCount76.value = parseInt(likeCount76.value) + 1;
+}
+
+function count77(){
+  dislikeCount77.value = parseInt(dislikeCount77.value) + 1;
+}
+function count78(){
+  likeCount78.value = parseInt(likeCount78.value) + 1;
+}
+
+function count79(){
+  dislikeCount79.value = parseInt(dislikeCount79.value) + 1;
+}
+function count80(){
+  likeCount80.value = parseInt(likeCount80.value) + 1;
+}
+
+function count81(){
+  dislikeCount81.value = parseInt(dislikeCount81.value) + 1;
+}
+
+function count82(){
+  likeCount82.value = parseInt(likeCount82.value) + 1;
+}
+
+function count83(){
+  dislikeCount83.value = parseInt(dislikeCount83.value) + 1;
+}
+
+function count84(){
+  likeCount84.value = parseInt(likeCount84.value) + 1;
+}
+
+function count85(){
+  dislikeCount85.value = parseInt(dislikeCount85.value) + 1;
+}
+
+
+function count86(){
+  likeCount86.value = parseInt(likeCount86.value) + 1;
+}
+
+function count87(){
+  dislikeCount87.value = parseInt(dislikeCount87.value) + 1;
+}
+
+
+function count88(){
+  likeCount88.value = parseInt(likeCount88.value) + 1;
+}
+
+function count89(){
+  dislikeCount89.value = parseInt(dislikeCount89.value) + 1;
+}
+
+function count90(){
+  likeCount90.value = parseInt(likeCount90.value) + 1;
+}
+
+function count91(){
+  dislikeCount91.value = parseInt(dislikeCount91.value) + 1;
+}
+
+function count92(){
+  likeCount92.value = parseInt(likeCount92.value) + 1;
+}
+
+function count93(){
+  dislikeCount93.value = parseInt(dislikeCount93.value) + 1;
+}
+
+function count94(){
+  likeCount94.value = parseInt(likeCount94.value) + 1;
+}
+
+function count95(){
+  dislikeCount95.value = parseInt(dislikeCount95.value) + 1;
+}
+
+function count96(){
+  likeCount96.value = parseInt(likeCount96.value) + 1;
+}
+
+function count97(){
+  dislikeCount97.value = parseInt(dislikeCount97.value) + 1;
+}
+
+function count98(){
+  likeCount98.value = parseInt(likeCount98.value) + 1;
+}
+
+function count99(){
+  dislikeCount99.value = parseInt(dislikeCount99.value) + 1;
+}
+function count100(){
+  likeCount100.value = parseInt(likeCount100.value) + 1;
+}
+
+function count101(){
+  dislikeCount101.value = parseInt(dislikeCount101.value) + 1;
+}
+
+function count102(){
+  likeCount102.value = parseInt(likeCount102.value) + 1;
+}
+
+function count103(){
+  dislikeCount103.value = parseInt(dislikeCount103.value) + 1;
+}
+
+function count104(){
+  likeCount104.value = parseInt(likeCount104.value) + 1;
+}
+
+function count105(){
+  dislikeCount105.value = parseInt(dislikeCount105.value) + 1;
+}
+
+function count106(){
+  likeCount106.value = parseInt(likeCount106.value) + 1;
+}
+
+function count107(){
+  dislikeCount107.value = parseInt(dislikeCount107.value) + 1;
+}
+
+function count108(){
+  likeCount108.value = parseInt(likeCount108.value) + 1;
+}
+
+function count109(){
+  dislikeCount109.value = parseInt(dislikeCount109.value) + 1;
+}
+
+function count110(){
+  likeCount110.value = parseInt(likeCount110.value) + 1;
+}
+
+function count111(){
+  dislikeCount111.value = parseInt(dislikeCount111.value) + 1;
+}
+
+function count112(){
+  likeCount112.value = parseInt(likeCount112.value) + 1;
+}
+
+function count113(){
+  dislikeCount113.value = parseInt(dislikeCount113.value) + 1;
+}
+
+
+function count114(){
+  likeCount114.value = parseInt(likeCount114.value) + 1;
+}
+
+function count115(){
+  dislikeCount115.value = parseInt(dislikeCount115.value) + 1;
+}
+
+function count116(){
+  likeCount116.value = parseInt(likeCount116.value) + 1;
+}
+
+function count117(){
+  dislikeCount117.value = parseInt(dislikeCount117.value) + 1;
+}
+
+function count118(){
+  likeCount118.value = parseInt(likeCount118.value) + 1;
+}
+
+function count19(){
+  dislikeCount119.value = parseInt(dislikeCount119.value) + 1;
+}
+
+function count120(){
+  likeCount120.value = parseInt(likeCount120.value) + 1;
+}
+
+function count121(){
+  dislikeCount121.value = parseInt(dislikeCount121.value) + 1;
+}
+
+function count122(){
+  likeCount122.value = parseInt(likeCount122.value) + 1;
+}
+
+function count123(){
+  dislikeCount123.value = parseInt(dislikeCount123.value) + 1;
+}
+
+function count124(){
+  likeCount124.value = parseInt(likeCount124.value) + 1;
+}
+
+function count125(){
+  dislikeCount125.value = parseInt(dislikeCount125.value) + 1;
+}
+
+function count126(){
+  likeCount126.value = parseInt(likeCount126.value) + 1;
+}
+
+function count127(){
+  dislikeCount127.value = parseInt(dislikeCount127.value) + 1;
+}
+
+function count128(){
+  likeCount128.value = parseInt(likeCount128.value) + 1;
+}
+
+function count129(){
+  dislikeCount129.value = parseInt(dislikeCount129.value) + 1;
+}
+
+function count130(){
+  likeCount130.value = parseInt(likeCount130.value) + 1;
+}
+
+function count131(){
+  dislikeCount131.value = parseInt(dislikeCount131.value) + 1;
+}
+
+function count132(){
+  likeCount132.value = parseInt(likeCount132.value) + 1;
+}
+
+function count133(){
+  dislikeCount133.value = parseInt(dislikeCount133.value) + 1;
+}
+
+  
+function count134(){
+likeCount134.value = parseInt(likeCount134.value) + 1;
+}
+
+function count135(){
+dislikeCount135.value = parseInt(dislikeCount135.value) + 1;
+}
+
+function count136(){
+likeCount136.value = parseInt(likeCount136.value) + 1;
+}
+
+function count137(){
+dislikeCount137.value = parseInt(dislikeCount137.value) + 1;
+}
+
+function count138(){
+likeCount138.value = parseInt(likeCount138.value) + 1;
+}
+
+function count139(){
+dislikeCount139.value = parseInt(dislikeCount139.value) + 1;
+}
+
+function count140(){
+likeCount140.value = parseInt(likeCount140.value) + 1;
+}
+
+function count141(){
+dislikeCount141.value = parseInt(dislikeCount141.value) + 1;
+}
+
+function count142(){
+likeCount142.value = parseInt(likeCount142.value) + 1;
+}
+
+function count143(){
+dislikeCount143.value = parseInt(dislikeCount143.value) + 1;
+}
+
+function count144(){
+likeCount144.value = parseInt(likeCount144.value) + 1;
+}
+
+function count145(){
+dislikeCount145.value = parseInt(dislikeCount145.value) + 1;
+}
+
+function count146(){
+likeCount146.value = parseInt(likeCount146.value) + 1;
+}
+
+function count147(){
+dislikeCount147.value = parseInt(dislikeCount147.value) + 1;
+}
+
+function count148(){
+likeCount148.value = parseInt(likeCount148.value) + 1;
+}
+
+function count149(){
+dislikeCount149.value = parseInt(dislikeCount149.value) + 1;
+}
+
+function count150(){
+likeCount150.value = parseInt(likeCount150.value) + 1;
+}
+
+function count151(){
+dislikeCount151.value = parseInt(dislikeCount151.value) + 1;
+}
+
+function count152(){
+likeCount152.value = parseInt(likeCount152.value) + 1;
+}
+
+function count153(){
+dislikeCount153.value = parseInt(dislikeCount153.value) + 1;
+}
+
+function count154(){
+likeCount154.value = parseInt(likeCount154.value) + 1;
+}
+
+function count155(){
+dislikeCount155.value = parseInt(dislikeCount155.value) + 1;
+}
+
+function count156(){
+likeCount156.value = parseInt(likeCount156.value) + 1;
+}
+
+function count157(){
+dislikeCount157.value = parseInt(dislikeCount157.value) + 1;
+}
+
+function count158(){
+likeCount158.value = parseInt(likeCount158.value) + 1;
+}
+
+function count159(){
+dislikeCount159.value = parseInt(dislikeCount159.value) + 1;
+}
+
+function count160(){
+likeCount160.value = parseInt(likeCount160.value) + 1;
+}
+
+function count161(){
+dislikeCount161.value = parseInt(dislikeCount161.value) + 1;
+}
+
+function count162(){
+likeCount162.value = parseInt(likeCount162.value) + 1;
+}
+
+function count163(){
+dislikeCount163.value = parseInt(dislikeCount163.value) + 1;
+}
+
+function count164(){
+likeCount164.value = parseInt(likeCount164.value) + 1;
+}
+
+function count165(){
+dislikeCount165.value = parseInt(dislikeCount165.value) + 1;
+}
+
+function count166(){
+likeCount166.value = parseInt(likeCount166.value) + 1;
+}
+
+function count167(){
+dislikeCount167.value = parseInt(dislikeCount167.value) + 1;
+}
+
+function count168(){
+likeCount168.value = parseInt(likeCount168.value) + 1;
+}
+
+function count169(){
+dislikeCount169.value = parseInt(dislikeCount169.value) + 1;
+}
+function count170(){
+likeCount170.value = parseInt(likeCount170.value) + 1;
+}
+
+function count171(){
+dislikeCount171.value = parseInt(dislikeCount171.value) + 1;
+}
+
+function count172(){
+likeCount172.value = parseInt(likeCount172.value) + 1;
+}
+
+function count173(){
+dislikeCount173.value = parseInt(dislikeCount173.value) + 1;
+}
+
+function count174(){
+likeCount174.value = parseInt(likeCount174.value) + 1;
+}
+
+function count175(){
+dislikeCount175.value = parseInt(dislikeCount175.value) + 1;
+}
+
+function count176(){
+likeCount176.value = parseInt(likeCount176.value) + 1;
+}
+
+function count177(){
+dislikeCount177.value = parseInt(dislikeCount177.value) + 1;
+}
+function count178(){
+likeCount178.value = parseInt(likeCount178.value) + 1;
+}
+
+function count179(){
+dislikeCount179.value = parseInt(dislikeCount179.value) + 1;
+}
+function count180(){
+likeCount180.value = parseInt(likeCount180.value) + 1;
+}
+
+function count181(){
+dislikeCount181.value = parseInt(dislikeCount181.value) + 1;
+}
+
+function count182(){
+likeCount182.value = parseInt(likeCount182.value) + 1;
+}
+
+function count183(){
+dislikeCount183.value = parseInt(dislikeCount183.value) + 1;
+}
+
+function count184(){
+likeCount184.value = parseInt(likeCount184.value) + 1;
+}
+
+function count185(){
+dislikeCount185.value = parseInt(dislikeCount185.value) + 1;
+}
+
+
+function count186(){
+likeCount186.value = parseInt(likeCount186.value) + 1;
+}
+
+function count187(){
+dislikeCount187.value = parseInt(dislikeCount187.value) + 1;
+}
+
+
+function count188(){
+likeCount188.value = parseInt(likeCount188.value) + 1;
+}
+
+function count189(){
+dislikeCount189.value = parseInt(dislikeCount189.value) + 1;
+}
+
+function count190(){
+likeCount190.value = parseInt(likeCount190.value) + 1;
+}
+
+function count191(){
+dislikeCount191.value = parseInt(dislikeCount191.value) + 1;
+}
+
+function count192(){
+likeCount192.value = parseInt(likeCount192.value) + 1;
+}
+
+function count193(){
+dislikeCount193.value = parseInt(dislikeCount193.value) + 1;
+}
+
+function count194(){
+likeCount194.value = parseInt(likeCount194.value) + 1;
+}
+
+function count195(){
+dislikeCount195.value = parseInt(dislikeCount195.value) + 1;
+}
+
+function count196(){
+likeCount196.value = parseInt(likeCount196.value) + 1;
+}
+
+function count197(){
+dislikeCount197.value = parseInt(dislikeCount197.value) + 1;
+}
+
+function count198(){
+likeCount198.value = parseInt(likeCount198.value) + 1;
+}
+
+function count199(){
+dislikeCount199.value = parseInt(dislikeCount199.value) + 1;
+}
+function count200(){
+likeCount200.value = parseInt(likeCount200.value) + 1;
+}
+
+function count201(){
+dislikeCount201.value = parseInt(dislikeCount201.value) + 1;
+}
+
+function count202(){
+  likeCount202.value = parseInt(likeCount202.value) + 1;
+}
+
+function count203(){
+  dislikeCount203.value = parseInt(dislikeCount203.value) + 1;
+}
+
+
+function count204(){
+  likeCount204.value = parseInt(likeCount204.value) + 1;
+}
+
+function count205(){
+  dislikeCount205.value = parseInt(dislikeCount205.value) + 1;
+}
+
+function count206(){
+  likeCount206.value = parseInt(likeCount206.value) + 1;
+}
+
+function count207(){
+  dislikeCount207.value = parseInt(dislikeCount207.value) + 1;
+}
+
+function count208(){
+  likeCount208.value = parseInt(likeCount208.value) + 1;
+}
+
+function count209(){
+  dislikeCount209.value = parseInt(dislikeCount209.value) + 1;
+}
+
+function count210(){
+  likeCount210.value = parseInt(likeCount210.value) + 1;
+}
+
+function count211(){
+  dislikeCount211.value = parseInt(dislikeCount211.value) + 1;
+}
+
+function count212(){
+  likeCount212.value = parseInt(likeCount212.value) + 1;
+}
+
+function count213(){
+  dislikeCount213.value = parseInt(dislikeCount213.value) + 1;
+}
+
+
+function count214(){
+  likeCount214.value = parseInt(likeCount214.value) + 1;
+}
+
+function count215(){
+  dislikeCount215.value = parseInt(dislikeCount215.value) + 1;
+}
+
+function count216(){
+  likeCount216.value = parseInt(likeCount216.value) + 1;
+}
+
+function count217(){
+  dislikeCount217.value = parseInt(dislikeCount217.value) + 1;
+}
+
+function count218(){
+  likeCount218.value = parseInt(likeCount218.value) + 1;
+}
+
+function count219(){
+  dislikeCount219.value = parseInt(dislikeCount219.value) + 1;
+}
+
+function count220(){
+  likeCount220.value = parseInt(likeCount220.value) + 1;
+}
+
+function count221(){
+  dislikeCount221.value = parseInt(dislikeCount221.value) + 1;
+}
+
+function count222(){
+  likeCount222.value = parseInt(likeCount222.value) + 1;
+}
+
+function count223(){
+  dislikeCount223.value = parseInt(dislikeCount223.value) + 1;
+}
